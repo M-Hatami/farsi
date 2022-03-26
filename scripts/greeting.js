a23a7e9358a26bca0b37a27d7cf73f32a6fb8f37
MH <32916645+M-Hatami@users.noreply.github.com>
2022-03-26 12:26:39 +1100

    latest changes

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -1,18 +1,17 @@
 function greet() {
-    let today = new Date();
-    let hourNow = today.getHours();
-    const elGr = document.getElementById('greeting')
+  let today = new Date();
+  let hourNow = today.getHours();
+  const elGr = document.getElementById("greeting");
 
-    if (hourNow > 18) {
-        elGr.innerHTML = 'سلام، شب بخیر';
-    } else if (hourNow > 12) {
-        elGr.innerHTML = 'سلام، عصر بخیر';
-    } else if (hourNow > 0) {
-        elGr.innerHTML = 'سلام، صبح بخیر'
-    } else {
-        elGr.innerHTML = 'سلام، خوش آمدی';
-    }
+  if (hourNow > 18) {
+    elGr.innerHTML = "سلام، شب بخیر";
+  } else if (hourNow > 12) {
+    elGr.innerHTML = "سلام، عصر بخیر";
+  } else if (hourNow > 0) {
+    elGr.innerHTML = "سلام، صبح بخیر";
+  } else {
+    elGr.innerHTML = "سلام، خوش آمدی";
+  }
 }
 
-
-window.addEventListener('load', greet)
+window.addEventListener("load", greet);
